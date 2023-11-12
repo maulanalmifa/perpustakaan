c7544cde3ed653db894b2b83eeac7f1a38092c50
maulanalmifa <lanal.fahmi27@gmail.com>
2023-11-12 11:20:38 +0700

    Menambahkan file index. masih kosong

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Dashboard Pustakawan</title>
+</head>
+<body>
+<h1>Dashboard</h1>
+<a href="anggota.php">Data Anggota</a>
+<a href="buku.php">Buku</a>
+<a href="peminjaman.php">Peminjaman</a>
+</body>
+</html>
